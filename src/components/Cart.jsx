62ec8730a0c3a260d907953573deff34648f0891
Dika Wicaksono <dhkwck00@gmail.com>
2023-10-30 20:41:18 +0700

    add react context and doing some refactor

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,9 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import { useContext, useState } from "react";
+import { CartContext } from "../store/shopping-cart-context";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const { items } = useContext(CartContext)
+  
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
